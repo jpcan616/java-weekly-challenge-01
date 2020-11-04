31af7630047d12dda6e6602c1a15b4e7f4766186
jpcan616 <juliacantave@hotmail.com>
2020-11-03 20:09:24 -0500

    Tue challenge

--- src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
+++ src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
@@ -1,12 +1,14 @@
 package com.codedifferently.day01Tuesday;
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 public class BasicArrayUtils {
     /**
      * @param stringArray an array of String objects
      * @return the first element in the array
      */
     public static String getFirstElement(String[] stringArray) {
-        return null;
+        return stringArray[0];
     }
 
     /**
@@ -14,7 +16,7 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+        return stringArray [1];
     }
 
     /**
@@ -22,7 +24,14 @@ public class BasicArrayUtils {
      * @return stringArray with the elements in reverse order
      */
     public static String[] reverse(String[] stringArray) {
-        return null;
+        int counter = 0;
+        String[] reverseString = new String[stringArray.length];
+        for(int i =stringArray.length -1; i>= 0; i--) {
+            reverseString[counter] = stringArray [i];
+        counter++; }
+
+                return reverseString;
+
     }
 
     /**
@@ -30,6 +39,11 @@ public class BasicArrayUtils {
      * @return String made up of the first character in each element of stringArray
      */
     public static String getFirstLetterOfEachElement(String[] stringArray) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < stringArray.length; i++) {
+            char firstLetter = stringArray[i].charAt(0);
+            stringBuilder.append(firstLetter);
+        }
+        return stringBuilder.toString();
     }
 }
